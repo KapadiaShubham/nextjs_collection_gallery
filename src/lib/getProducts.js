cc5d46e762111263d989f21388dc0cbf7b4c3bc5
Shubham Kapadia <73032350+KapadiaShubham@users.noreply.github.com>
2025-07-07 09:13:43 +0530

    Update getProducts.js

--- src/lib/getProducts.js
+++ src/lib/getProducts.js
@@ -1,5 +1,6 @@
 // src/lib/getProducts.js
 export async function getAllProducts() {
+  console.log("BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`, {
     next: { revalidate: 60*60*24 }, // Revalidate every N seconds
     // cache: "no-store", // Important if you want fresh data every time
